c152f9f7d151cf8e99ff43180090611327952fff
kor0tkov <48534370+kor0tkov@users.noreply.github.com>
2019-06-25 05:27:18 +0500

    fix menu, add link

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,9 +3,21 @@ $('.menu-btn').on('click', function(e) {
   $('.menu').toggleClass('menu_active');
   $('.menu-btn').toggleClass('menu_active');
   $('.content').toggleClass('content_active');
+  $('html').toggleClass('hidden-overflow');
 })
 
-
-$('.menu-item').on('click', function(e) { 
+$('.menu-item_link').on('click', function(e) {
+  e.preventDefault();
   $('.menu_active').removeClass('menu_active');
+  let id = $(this).attr('href');
+  let top = $(id).offset().top;
+  $('html').removeClass('hidden-overflow');
+  $('body,html').animate({scrollTop: top}, 500);
+})
+
+$('.feedback_form-link').on('click', function(e){
+  e.preventDefault();
+  let id = $(this).attr('href');
+  let top = $(id).offset().top;
+  $('html').animate({scrollTop: top}, 750);
 })
